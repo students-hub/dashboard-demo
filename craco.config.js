392d8e814775beaf308baf0918371262f41918ad
jhonlee <lilimul@yeah.net>
2021-05-28 15:33:16 +0800

    feat(all): update craco setting

--- craco.config.js
+++ craco.config.js
@@ -6,4 +6,22 @@ module.exports = {
         ],
       },
     },
+    webpack: (config) => {
+      config.output.library = `react-dashboard`;
+      config.output.libraryTarget = 'umd';
+      config.output.jsonpFunction = `webpackJsonp_react`;
+      config.output.globalObject = 'window';
+      return config;
+    },
+    devServer: (_) => {
+      const config = _;
+      config.headers = {
+        'Access-Control-Allow-Origin': '*',
+      };
+      config.historyApiFallback = true;
+      config.hot = false;
+      config.watchContentBase = false;
+      config.liveReload = false;
+      return config;
+    },
   }
